c6e3fbec4b12683b8e6e904bb8d30d3bcbb559c3
ibukamshindi <ibukamshindi@gmail.com>
2020-08-23 18:34:25 +0300

    add label for date, class quote and ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotes-app';
+  quote:Quote[] = [
+    {id:1., quote: '"I would rather die than resign"',author: 'Amos Kimunya',submittedby:'Anonymous', date: '01/04/2014'},
+    {id:2., quote:'"No matter where you are from, your dreams are valid"',author: 'Lupita Nyongo',submittedby:'Anonymous', date: '01/05/2015'},
+    {id:3., quote: '"You do not respond to a mosquito bite with a hammer."',author: 'Patrick Lumumba',submittedby:'A.N. Other', date: '03/04/2014'},
+    
+  ];
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,7 @@
 export class Quote {
   id: number;
+  quote: string
   author: string;
-
+  submittedby: string;
+  date: string;
 }

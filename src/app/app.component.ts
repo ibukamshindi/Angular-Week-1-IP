74795f489ec38aca93e863f4e612ffd3b31eea8f
ibukamshindi <ibukamshindi@gmail.com>
2020-08-24 07:52:45 +0300

    add logic to quote-diplay from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,12 @@ import {Quote} from './quote';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  quote:Quote[] = [
-    {id:1., quote: '"I would rather die than resign"',author: 'Amos Kimunya',submittedby:'Anonymous', date: '01/04/2014'},
-    {id:2., quote:'"No matter where you are from, your dreams are valid"',author: 'Lupita Nyongo',submittedby:'Anonymous', date: '01/05/2015'},
-    {id:3., quote: '"You do not respond to a mosquito bite with a hammer."',author: 'Patrick Lumumba',submittedby:'A.N. Other', date: '03/04/2014'},
-  ];
+//   // quote:Quote[] = [
+//   //   {id:1., quote: '"I would rather die than resign"',author: 'Amos Kimunya',submittedby:'Anonymous', date: '01/04/2014'},
+//   //   {id:2., quote:'"No matter where you are from, your dreams are valid"',author: 'Lupita Nyongo',submittedby:'Anonymous', date: '01/05/2015'},
+//   //   {id:3., quote: '"You do not respond to a mosquito bite with a hammer."',author: 'Patrick Lumumba',submittedby:'A.N. Other', date: '03/04/2014'},
+//   // ];
+// }
 }
--- src/app/quote-display/quote-display.component.ts
+++ src/app/quote-display/quote-display.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
 
 @Component({
-  selector: 'app-quote-display',
+  selector: ' app-quote-display',
   templateUrl: './quote-display.component.html',
   styleUrls: ['./quote-display.component.css']
 })
 export class QuoteDisplayComponent implements OnInit {
 
+    quote:Quote[] = [
+      {id:1., quote: '"I would rather die than resign"',author: 'Amos Kimunya',submittedby:'Anonymous', date: '01/04/2014'},
+      {id:2., quote:'"No matter where you are from, your dreams are valid"',author: 'Lupita Nyongo',submittedby:'Anonymous', date: '01/05/2015'},
+      {id:3., quote: '"You do not respond to a mosquito bite with a hammer."',author: 'Patrick Lumumba',submittedby:'A.N. Other', date: '03/04/2014'},
+    ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }

3c2868f49d72bfca6a8b58227deb0d7c25e072a6
ibukamshindi <ibukamshindi@gmail.com>
2020-08-24 06:08:11 +0300

    removed quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,5 @@ export class AppComponent {
     {id:1., quote: '"I would rather die than resign"',author: 'Amos Kimunya',submittedby:'Anonymous', date: '01/04/2014'},
     {id:2., quote:'"No matter where you are from, your dreams are valid"',author: 'Lupita Nyongo',submittedby:'Anonymous', date: '01/05/2015'},
     {id:3., quote: '"You do not respond to a mosquito bite with a hammer."',author: 'Patrick Lumumba',submittedby:'A.N. Other', date: '03/04/2014'},
-    
   ];
 }

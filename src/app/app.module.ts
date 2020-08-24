f068eed0ad9dac7cad548e76776f7de607566eb2
ibukamshindi <ibukamshindi@gmail.com>
2020-08-24 08:31:00 +0300

    add quote-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteFormComponent,
     QuoteDisplayComponent,
+    QuoteDetailComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
